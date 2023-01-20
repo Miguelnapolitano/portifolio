11335c6058ac8099d89b2943b4bdc0b0d17d2bb8
Miguel <miguel.napolitano@live.com>
2023-01-19 21:36:06 -0300

    feat: create home and about pages

---
+++ Scripts/data.js
@@ -0,0 +1,14 @@
+const dataBase = [
+    {
+        name: "Hamburgueria V2",
+        ano: 2023,
+        techs: ["html", "css", "JavaScript", "react"],
+        description: "A Hamburgueria V2 é um marketplace de uma lanchonete. Este projeto foi desenvolvido em react e usa algumas bibliotecas como: Styled Compenents e react toastfy.",
+        links:{
+            repository: "https://github.com/Kenzie-Academy-Brasil-Developers/react-entrega-hamburgueria-v2-Miguelnapolitano",
+            linkedin:"https://www.linkedin.com/posts/miguel-felipe-napolitano-70407776_ol%C3%A1-pessoal-mais-uma-vez-aqui-trazendo-activity-7002282030679908352-mN_f?utm_source=share&utm_medium=member_desktop",
+            site:"https://react-entrega-hamburgueria-v2-miguelnapolitano.vercel.app/",
+            img:"./Assets/hamburgueria_V2.png"
+        }
+    }
+]
